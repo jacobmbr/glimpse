44ecc5e0185a886b99f65716fa269fc87645a770
Jacob Romer <jmbrjmbr@gmail.com>
2016-04-27 11:18:35 -0400

    drawing circles & lines dynamically

--- src/cljs/modern_cljs/core.cljs
+++ src/cljs/modern_cljs/core.cljs
@@ -12,6 +12,8 @@
 (devtools/install! [:sanity-hints :custom-formatters])
 (def log #(.log js/console %))
 
+(defonce data [["domain1", 3, true] ["domain2", 6, true]])
+
 (defn add-image-listeners []
   (do
     (evt/listen!
@@ -30,7 +32,7 @@
   []
   (if-not (by-id "ext-canvas-container")
     (do
-      (add-image-listeners)
+      ;(add-image-listeners)
       (append! (sel "body") (str
                               (html [:div#ext-canvas-container])
                               ;(html [:img#ext-image.ext-image.ext-canvas-slide {:src "img/t.png"}])))
@@ -44,26 +46,50 @@
              (refresh true)))
 
 (defn draw-circle
-  [x y r]
-  (let [form (js/Form. space)]
+  [form p r]
     (.. form
         (stroke "rgba(255,255,255,1)" 1)
         (fill false)
         (circle
           (..
             (js/Circle.
-              (js/Point.
-                (clj->js [x y]))) (setRadius r))))))
+              p) (setRadius r)))))
+(defn draw-line
+  [form from to]
+  (let [[x1 y1] from
+        [x2 y2] to]
+    (.. form
+      (stroke "white" 2)
+      (line (.. (js/Line. x1 y1) (to x2 y2))))))
+
+(defn draw-circle-with-line
+  [form from to]
+  (do 
+    (draw-circle form (js/Point. (clj->js to)) 5)
+    (draw-line form from to)))
+
+(defn dots-from-data
+  [data]
+  (let [w (.. space -size -x)
+        h (.. space -size -y)]
+  (reduce #(conj %1 (conj %2 (* (+ 1 (count %1)) (/ h (count data))))) [] data)))
 
 (defn draw
-  ([time]
+  ([time form w h]
+    (.clearRect (.-ctx space) 0 0 3000 3000)
+    (doall (map 
+             #(draw-circle-with-line form [0 (/ h 2)] [(/ w 2) (get % 3)]) 
+             (dots-from-data data)))
+    (.requestAnimationFrame js/window #(draw % form w h)))
+  ([form w h]
+   (println (dots-from-data data))
+  (draw 0 form w h)))
+
+(defn draw-loop
+  []
   (let [form (js/Form. space)
         w (.. space -size -x)
         h (.. space -size -y)]
-    (.clearRect (.-ctx space) 0 0 3000 3000)
-    (doall (map #(draw-circle (/ w %) (/ h 2) (* 9 %)) (range 100)))
-    (.requestAnimationFrame js/window draw)))
-  ([]
-  (do (draw 0))))
+    (draw form w h)))
 
-(draw)
+(draw-loop)
