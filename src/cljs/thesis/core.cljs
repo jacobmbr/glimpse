7460dce0a3658f0f19b6722cc6ce629ccc84d7b9
Jacob Romer <jmbrjmbr@gmail.com>
2016-04-27 18:04:30 -0400

    clean up draw loop

--- src/cljs/thesis/core.cljs
+++ src/cljs/thesis/core.cljs
@@ -1,15 +1,15 @@
 ;; create the main project namespace
 (ns thesis.core
   (:require-macros [hiccups.core :as hiccups :refer [html]]
-                   [cljs.core.async.macros :as m :refer [go]])
+                   [cljs.core.async.macros :as m :refer [go go-loop]])
   (:require [domina.core :refer [by-id value set-value! append! by-class]]
             [domina.css :refer [sel]]
-            [thesis.draw :refer [draw-entity]]
+            [thesis.draw :as draw :refer [draw-entity]]
             [domina.events :as evt]
             [devtools.core :as devtools]
             [reagent.core :as r]
-            [cljs.core.async :refer [chan close!]]
-            [timothypratley.reanimated.core :as anim]
+            [cljs.core.async :refer [chan close! <! >!]]
+            [thesis.animation :as anim]
             [hiccups.runtime :as hiccupsrt]))
 
 ;; enable cljs to print to the JS console of the browser
@@ -19,9 +19,13 @@
 
 (def data [["domain8", 1, true] ["domain8", 1, true] ["domain1", 3, true] ["domain2", 6, true]])
 
+(defonce resize-chan (chan))
+(defonce dim (r/atom {:w 0 :h 0}))
+
 
 (defn add-image-listeners []
   (do
+    (.. js/window (addEventListener "resize" (fn [e] (go (>! resize-chan "hu")))))
     (evt/listen!
     js/body
     :mousemove
@@ -38,6 +42,7 @@
   []
   (if-not (by-id "ext-canvas-container")
     (do
+      (swap! dim #(assoc % :w (.. js/window -innerWidth) :h (.. js/window -innerHeight)))
       (add-image-listeners)
       (append! (sel "body") (str
                               (html [:div#ext-canvas-container])
@@ -50,11 +55,15 @@
              (js/CanvasSpace.)
              (display "#ext-canvas-container")
              (refresh true)))
+(go-loop []
+  (<! resize-chan)
+  (swap! dim #(assoc % :w (.. js/window -innerWidth) :h (.. js/window -innerHeight)))
+  (recur))
 
 (defn dots-from-data
   [data]
-  (let [w (.. space -size -x)
-        h (.. space -size -y)]
+  (let [w (:w @dim)
+        h (:h @dim)]
   (reduce 
     #(conj %1 (conj %2 (* (+ 0.5 (count %1)) (/ h (count data)))))
     [] data)))
@@ -75,29 +84,29 @@
     (swap! shift #(condp = %
                      0 1
                      1 0))
-    (loop-go)
+    ;(loop-go)
     ))
 (loop-go)
 
 (defn draw
   ([time form w h center dotsdata]
-    (.clearRect (.-ctx space) 0 0 3000 3000)
+    (.clearRect (.-ctx space) 0 0 w h)
+    #_(doall (map 
+           #(draw/draw-text form (js/Point. 200 (get % 3)) (get % 0) 25)
+           dotsdata))
     (doall (map 
              #(draw-entity 
                 form 
                 [(/ w 2) (/ h 2)] 
-                [(get % 3) (get % 3)] 
+                [(- w 100) (get % 3)] 
                 center
                 @rot)
              dotsdata))
-    (.requestAnimationFrame js/window #(draw % form w h center dotsdata))))
+    (.requestAnimationFrame js/window #(draw % form)))
+  ([time form]
+   (let [h (:h @dim)
+         w (:w @dim)]
+    (draw time form w h (.. (js/Vector. 0 (/ h 2)) (add 1 1)) ;add moouse parallax
+                       (dots-from-data data)))))
 
-(defn draw-loop
-  []
-  (let [form (js/Form. space)
-        w (.. space -size -x)
-        h (.. space -size -y)
-        center (js/Vector. (/ w 2) (/ h 2))]
-    (draw 0 form w h center (dots-from-data data))))
-
-(draw-loop)
+(draw 0 (js/Form. space))
