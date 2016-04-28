7a8d9c1ecca3efd5fd33e58d7b2b82ddb7472c6c
Jacob Romer <jmbrjmbr@gmail.com>
2016-04-27 22:16:33 -0400

    programmatisches malen

--- src/cljs/thesis/core.cljs
+++ src/cljs/thesis/core.cljs
@@ -4,7 +4,7 @@
                    [cljs.core.async.macros :as m :refer [go go-loop]])
   (:require [domina.core :refer [by-id value set-value! append! by-class]]
             [domina.css :refer [sel]]
-            [thesis.draw :as draw :refer [draw-entity]]
+            [thesis.draw :as draw :refer [draw-entity draw-text]]
             [domina.events :as evt]
             [devtools.core :as devtools]
             [reagent.core :as r]
@@ -18,24 +18,24 @@
 (def log #(.log js/console %))
 
 (def app-db (r/atom {:screen {:w 0 :h 0}
-                     :data [["domain8", 1, true]
-                           ["domain8", 1, true]
-                           ["domain1", 3, true]
-                           ["facebook", 3, true]
-                           ["domain1", 3, true]
-                           ["domain1", 3, true]
-                           ["domain1", 3, true]
-                           ["domain1", 15, true]
-                           ["myspace", 15, true]
-                           ["domain1", 15, true]
-                           ["domain1", 3, true]
-                           ["domain1", 3, true]
-                           ["domain1", 3, true]
-                           ["domain1", 7, true]
-                           ["domain1", 7, true]
-                           ["domain1", 7, true]
-                           ["domain1", 3, true]
-                           ["domain2", 6, true]]
+                     :data [{:pos {:x 0 :y 0} :data ["domain8", 1, true]}
+                           {:pos {:x 19 :y 0} :data ["domain8", 1, true]}
+                           {:pos {:x 49 :y 0} :data ["domain1", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["facebook", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 15, true]}
+                           {:pos {:x 19 :y 0} :data ["myspace", 15, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 15, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 7, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 7, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 7, true]}
+                           {:pos {:x 19 :y 0} :data ["domain1", 3, true]}
+                           {:pos {:x 19 :y 0} :data ["domain2", 6, true]}]
                      :offset 0
                      :shift 0
                      :current-page 1}))
@@ -44,12 +44,33 @@
 (def shift (r/cursor app-db [:shift]))
 (def offset (r/cursor app-db [:offset]))
 
+(def reactions (map-indexed #(anim/spring (r/cursor app-db [:data %1 :pos :x])) @data))
 
 (def rot (anim/spring shift))
 (def ospring (anim/spring offset))
 
 (defonce resize-chan (chan))
 (def state-chan (chan))
+
+(defn randomize-data
+  []
+  (swap! data #(update-in % assoc {:pos {:x (rand)}})))
+
+(defn get-springs
+  [data]
+  (reduce #(conj %1 {:no (count %1)
+                     :x (anim/spring (r/cursor app-db [:data (count %1) :pos :x]))
+                     :y (anim/spring (r/cursor app-db [:data (count %1) :pos :y]))})
+  [] data))
+
+(def springs (get-springs @data))
+
+(defn draw-element
+  [form]
+  (let []
+    (do 
+      (doall (map #(draw-text form (js/Point. (+ 200 @(get % :x)) (+ 200 @(get % :y)))  (get-in @data [(get % :no) :data 0]) 12) springs))
+      #_(swap! me #(+ % 200) ))))
 
 (defn switch-page
   []
@@ -67,14 +88,15 @@
                                0 1
                                1 -1
                                -1 0))
-             "hu" (switch-page))
+             "hu" (switch-page)
+             "randomize" (do (randomize-data)))
            (recur)))))
 (switch-state state-chan)
 
 (defn add-image-listeners []
   (do
     (.. js/window (addEventListener "resize" (fn [e] (go (>! resize-chan "hu")))))
-    (.. js/window (addEventListener "mousedown" (fn [e] (go (>! state-chan "hu")))))
+    (.. js/window (addEventListener "mousedown" (fn [e] (go (>! state-chan "randomize")))))
     (.. js/window (addEventListener "keydown" (fn [e] (go (>! state-chan "page")))))
     (evt/listen!
     js/body
@@ -90,14 +112,15 @@
 
 (defn setup
   []
-  (if-not (by-id "ext-canvas-container")
+  (do
+    (swap! dim #(assoc % :w (.. js/window -innerWidth) :h (.. js/window -innerHeight)))
+    (if-not (by-id "ext-canvas-container")
     (do
-      (swap! dim #(assoc % :w (.. js/window -innerWidth) :h (.. js/window -innerHeight)))
       (add-image-listeners)
       (append! (sel "body") (str
                               (html [:div#ext-canvas-container])
                               ;(html [:img#ext-image.ext-image.ext-canvas-slide {:src "img/t.png"}])))
-                              )))))
+                              ))))))
 
 (setup)
 
@@ -115,14 +138,12 @@
   (let [w (:w @dim)
         h (:h @dim)
         data @datac]
-  (reduce
-    #(conj %1 (conj %2 (* (+ 0.5 (count %1)) (/ h (count data)))))
-    [] data)))
+        (map-indexed #(update-in %2 [:pos :y] + (* (+ 0.5 %1) (/ h (count data)))) data)))
 
 (defn sc-overview
   [os form hostnameDict]
   (doall (map
-         #(draw/draw-text form (js/Point. 200 (+ os (get % 3))) (get % 0) 25)
+         #(draw/draw-text form (js/Point. (get-in % [:pos :x]) (+ os (get-in % [:pos :y]))) (get-in % [:data 0]) 25)
          hostnameDict)))
 
 (defn sc-satellites
@@ -130,12 +151,13 @@
   (doall (map #(draw-entity
             form
             [(/ w 2) (/ h 2)]
-            [(- w 100) (+ osf (get % 3))]
+            [(- w 100) (+ osf (:pos :y %))]
             (.. center (add 0 os))
             @rot
             %
             os)
            dotsdata)))
+
 
 (defn draw
   ([time form w h center dotsdata]
@@ -143,6 +165,7 @@
          osf @offset]
     (.clearRect (.-ctx space) 0 0 w h)
 
+    (draw-element form)
     (sc-overview os form dotsdata)
     (sc-satellites os osf form center h w dotsdata)
 
