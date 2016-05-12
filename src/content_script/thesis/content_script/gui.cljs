eddbeb1c79e3058839c1d949f01136b4a4eccd60
Jacob Romer <jmbrjmbr@gmail.com>
2016-05-12 00:49:44 -0400

    programmatic animation working again

--- src/content_script/thesis/content_script/gui.cljs
+++ src/content_script/thesis/content_script/gui.cljs
@@ -5,6 +5,7 @@
             [thesis.content-script.animation :as anim]
             [chromex.logging :refer-macros [log]]
             [domina.events :as evt]
+            [domina.css :refer [sel]]
             [domina.core :refer [by-id value set-value! destroy! append! by-class]]))
 
 (def img-data (r/atom nil))
@@ -13,7 +14,6 @@
 (def mouse (r/atom [0 0]))
 (def mouseX (reaction (first @mouse)))
 (def mouseY (reaction (peek @mouse)))
-
 
 
 (defn satellite [el]
@@ -33,13 +33,13 @@
         (fn [el]
           [:div
            [:h1 {:class "ext-h1" 
-                 :style {:position "absolute" :left (+ 20 @mouseX) :top (+ @ysp @mouseY)
+                 :style {:position "absolute" :left (+ 20 @xsp) :top (+ @ysp @mouseY)
                          :font-size (str @fssp "px")
                          :font-weight "bold"
                          :background-color "transparent"
                          ;:transform (str "translate(" @xsp "px," @ysp "px)")
                          }} (str text)
-           (for [i (range dots)] ^{:key i} [:span i])]])})))
+           ]])})))
 
 (defn satellites []
   [:div {:style {:position "absolute" :left "0" :top "0"}}
@@ -50,7 +50,7 @@
   (let [tilt (r/atom 0)
         rotation (anim/spring tilt)
         flip (r/atom 1)
-        scale (anim/interpolate-to flip)
+        scale (anim/interpolate-to flip {:duration 800})
         w (.-innerWidth js/window)]
     (fn a-screenshot []
       [:div 
@@ -74,6 +74,9 @@
     (let [node (.. js/document (createElement "div"))
           el (.. js/document -body (appendChild node))
           div (set! (.-id el) "ext-canvas-container")]
+      ; OPTIONAL: Kill all scripts and iframes on the page. 
+      (destroy! (sel "script"))
+      (destroy! (sel "iframe"))
       (reset! img-data img)
       (->> (vec (map #(hash-map
                            :x 0
@@ -84,11 +87,17 @@
                            ) (vec tabdict)))
       (reset! data))
       (reset! dim [(.-innerWidth js/window) (.-innerHeight js/window)])
-      (.. js/window (addEventListener "mousemove" #(swap! mouse assoc 0 (.-clientX %) 1 (.-clientY %))))
+      ;(.. js/window (addEventListener "mousemove" #(swap! mouse assoc 0 (.-clientX %) 1 (.-clientY %))))
 
-      (js/setTimeout (fn [] (swap! data conj {:x 200 :font-size 10 :y 200 :text "huhu" :dots 25})) 2000)
+      ;(js/setTimeout (fn [] (swap! data conj {:x 200 :font-size 10 :y 200 :text "huhu" :dots 25})) 2000)
       (js/setTimeout (fn [] (swap! data (fn [e] (vec (map-indexed #(assoc %2 :x 400 :y (* 20 %1) ) e))))) 3000)
-      (js/setTimeout (fn [] (swap! data (fn [e] (vec (map #(update % :x + 200) e))))) 5000)
+      (log @data)
+
+      (js/setTimeout (fn [] (do 
+                              (log @data)
+                              (swap! data (fn [e] (vec (map-indexed #(assoc %2 :x 600 :y (* 20 %1) ) e)))))) 4000)
+
+
       (js/setTimeout (fn [] (swap! data (fn [e] (vec (map #(update % :x + 200) e))))) 6000)
 
       (r/render [root] (by-id "ext-canvas-container")))))
