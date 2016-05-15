547e0b8bc5afe40fe8942db2a3eaf6139ed52310
Jacob Romer <jmbrjmbr@gmail.com>
2016-05-15 01:12:19 -0400

    always get latest tabdict by spam and sliding buffered chan

--- src/content_script/thesis/content_script/core.cljs
+++ src/content_script/thesis/content_script/core.cljs
@@ -4,7 +4,7 @@
   (:require [thesis.content-script.draw :as draw :refer [draw-entity draw-text]]
             [devtools.core :as devtools]
             [thesis.dev]
-            [cljs.core.async :refer [chan close! <! >! put!]]
+            [cljs.core.async :refer [chan close! <! >! put! sliding-buffer]]
             [chromex.logging :refer-macros [log]]
             [chromex.protocols :refer [post-message!]]
             [chromex.ext.runtime :as runtime :refer-macros [connect]]
@@ -16,7 +16,7 @@
 
 ; -- a message loop ---------------------------------------------------------------------------------------------------------
 (def background-chan (atom nil))
-(def count-chan (chan))
+(def count-chan (chan (sliding-buffer 1)))
 (def counts (atom nil))
 
 (defn run-gui-loop! [msg-chan]
@@ -31,17 +31,18 @@
     "init" (let [img (.-img msg)
                  tabdict (.-tabdict msg)
                  core-chan (chan)]
-             ;(reset! gui-chan core-chan)
              (go 
                (if (nil? @counts)
                  (reset! counts (<! count-chan)))
-                 (gui/init! img tabdict (.-url msg) @counts core-chan)
-                 (run-gui-loop! core-chan)))
-    "new-request" (indicator/add-domain (.-tabdict msg))
+               (gui/init! img tabdict (.-url msg) @counts core-chan)
+               (run-gui-loop! core-chan)))
+    "new-request" (do 
+                    (log "new r")
+                    (post-message! @background-chan (clj->js {:reqtype "get-counts"}))
+                    (indicator/add-domain (.-tabdict msg)))
     "domains-info" (dispatch [:update-domain-info (.-res msg)])
     (if (= (.-restype msg) "distinct-domains") 
-      (put! count-chan (.-data msg))
-      (log msg))))
+      (put! count-chan (.-data msg)))))
 
 (defn run-message-loop! [message-channel]
   (log "CONTENT SCRIPT: starting message loop...")
